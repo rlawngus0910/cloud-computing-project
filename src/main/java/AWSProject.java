bbce06f56dd7a45d13acf35618fd2f431983da00
Kim Juhyun <rlawngus0910@naver.com>
2021-11-16 20:14:00 +0900

    feat : init method, print menu

--- src/main/java/AWSProject.java
+++ src/main/java/AWSProject.java
@@ -1,5 +1,57 @@
-public class AWSProject {
-    public static void main(String[] args) {
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 
+import java.util.Scanner;
+
+public class AWSProject {
+
+    static AmazonEC2 ec2;
+
+    private static void init() throws Exception {
+
+        ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                            "Please make sure that your credentials file is at the correct " +
+                            "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+
+        ec2 = AmazonEC2ClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withRegion("us-east-2")
+                .build();
+    }
+    public static void main(String[] args) throws Exception {
+
+        init();
+
+        Scanner menu = new Scanner(System.in);
+        Scanner id_string = new Scanner(System.in);
+        int number = 0;
+
+        while(true) {
+            System.out.println("                                                            ");
+            System.out.println("                                                            ");
+            System.out.println("------------------------------------------------------------");
+            System.out.println("           Amazon AWS Control Panel using SDK               ");
+            System.out.println("                                                            ");
+            System.out.println("  Cloud Computing, Computer Science Department              ");
+            System.out.println("                           at Chungbuk National University  ");
+            System.out.println("------------------------------------------------------------");
+            System.out.println("  1. list instance                2. available zones         ");
+            System.out.println("  3. start instance               4. available regions      ");
+            System.out.println("  5. stop instance                6. create instance        ");
+            System.out.println("  7. reboot instance              8. list images            ");
+            System.out.println("                                 99. quit                   ");
+            System.out.println("------------------------------------------------------------");
+            System.out.print("Enter an integer: ");
+        }
     }
 }
